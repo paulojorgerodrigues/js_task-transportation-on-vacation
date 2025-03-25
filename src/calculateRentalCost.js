d224b32e26d49fcb10f1c622dcab904462a070f7
paulojorgerodrigues <49186376+paulojorgerodrigues@users.noreply.github.com>
2025-03-25 01:50:55 +0000

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,18 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  let total = 0;
+  let discount = 0;
+
+  total = days * 40;
+
+  if( days >= 7 ) {
+    discount = 50;
+  } else if() {
+    discount = 20;
+  } 
+
+  return total - discount;
 }
 
 module.exports = calculateRentalCost;
